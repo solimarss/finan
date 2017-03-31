11240e4ffd9bb6870a88f55a0946e705caf0404d
Solimar Silva e Silva <solimar.silva@trt8.jus.br>
2017-03-31 11:53:12 -0300

    layout

--- src/main/java/br/com/solimar/finan/view/application/UserSession.java
+++ src/main/java/br/com/solimar/finan/view/application/UserSession.java
@@ -1,6 +1,11 @@
 package br.com.solimar.finan.view.application;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.enterprise.context.SessionScoped;
 
@@ -56,6 +61,38 @@ public class UserSession implements Serializable {
 
 	public void setAno(int ano) {
 		this.ano = ano;
+	}
+
+	public void nextMoth() {
+		mes = getMes() + 1;
+		if (mes > 12) {
+			mes = 1;
+			ano = ano + 1;
+		}
+	}
+
+	public void previousMoth() {
+		mes = getMes() - 1;
+		if (mes < 1) {
+			mes = 12;
+			ano = ano - 1;
+		}
+	}
+
+	public String getMesAsString() {
+		String s = "01/" + getMes() + "/2000";
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		Date dt;
+		try {
+			dt = df.parse(s);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return "";
+
+		}
+		DateFormat df2 = new SimpleDateFormat("MMMMM", new Locale("pt", "BR"));
+
+		return df2.format(dt);
 	}
 
 }

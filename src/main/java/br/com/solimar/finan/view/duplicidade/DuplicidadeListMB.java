8f19f8a1567a9f1762e72d884dc58c5c8926b6ea
Solimar Silva e Silva <solimar.silva@trt8.jus.br>
2017-05-19 09:02:22 -0300

    a

--- src/main/java/br/com/solimar/finan/view/duplicidade/DuplicidadeListMB.java
+++ src/main/java/br/com/solimar/finan/view/duplicidade/DuplicidadeListMB.java
@@ -4,14 +4,12 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.event.Observes;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.solimar.finan.business.LancamentoRN;
 import br.com.solimar.finan.entity.Lancamento;
-import br.com.solimar.finan.view.On;
 import br.com.solimar.finan.view.application.UIService;
 import br.com.solimar.finan.view.application.UserSession;
 
